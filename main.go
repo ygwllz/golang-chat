81526959f277a6d91ce8cd1285b619cc7ed78953
ygw <15234790216@163.com>
2023-05-22 22:25:07 -0400

    tune network

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
-import(
-	"fmt"
+
+import (
+	"github.com/gin-gonic/gin"
 )
 
-
-	
-func main () {
-	fmt.Println("ok")
+func main() {
+	r := gin.Default()
+	r.GET("/index", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello world!",
+		})
+	})
+	r.Run(":8080")
 }
